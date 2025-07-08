396abf9c391864f20d503eed1e3f188adcd287d0
macbook <macbook@macbooks-MBP.lan>
2025-07-08 22:49:07 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Header from "./components/Header.jsx";
 import "./App.css";
 import Home from "./pages/Home.jsx"
+
 function App() {
   return (
     <>
--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite"; 
+import react from "@vitejs/plugin-react"; 
+import tailwindcss from "tailwindcss";
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(),
-    tailwindcss(),
-  ],
-})
+// https://vitejs.dev/config/ 
+export default defineConfig({   
+    plugins: [react()],   
+    css: {
+        postcss: {
+            plugins: [tailwindcss()],
+        },   
+    }, 
+});
