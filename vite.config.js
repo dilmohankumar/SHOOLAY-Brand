9f131ae7141b6c5c6cdd7d5349b8f6079829c423
macbook <macbook@macbooks-MBP.lan>
2025-07-09 06:46:37 +0530

    commit

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,10 @@
-import { defineConfig } from "vite"; 
-import react from "@vitejs/plugin-react"; 
-import tailwindcss from "tailwindcss";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
-// https://vitejs.dev/config/ 
-export default defineConfig({   
-    plugins: [react()],   
-    css: {
-        postcss: {
-            plugins: [tailwindcss()],
-        },   
-    }, 
-});
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react(),
+    tailwindcss(),
+  ],
+})
