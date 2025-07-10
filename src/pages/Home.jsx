479abd16497390848d8c22678850c952e285f0c0
macbook <macbook@macbooks-MBP.lan>
2025-07-10 19:19:47 +0530

    shoee slider

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -12,13 +12,19 @@ import { productList } from "./qqq.jsx";
 import "swiper/css";
 import "swiper/css/pagination";
 
+
+// Import these at the top of your file
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+
 const Home = () => {
   const banners = [Banner, Banner2];
   const [products, setProducts] = useState(productList.products);
 
   return (
     <div>
-
       <div className="w-full h-[300px]">
         <Swiper
           modules={[Autoplay, Pagination]}
@@ -38,24 +44,50 @@ const Home = () => {
           ))}
         </Swiper>
       </div>
-{/* ---------------------3 Product -------------------------- */}
-          <div className="flex flex-col m-12  items-revert justify-between">
-          <div className=" grid grid-cols-4 gap-4 p-4  text-center">
-      {products.map((product) => (
-        <div key={product.id} className="rounded-[10px] border border:1px solid black flex flex-col items-center p-4 ">
+      {/* ---------------------3 Product -------------------------- */}
+      <div className="m-12">
+  <Slider className=""
+    dots={false}
+    infinite={true}
+    speed={1000}
+    slidesToShow={5}
+    slidesToScroll={1}
+    autoplay={true}
+    autoplaySpeed={2000} // 3 seconds
+    arrows={true}
+    responsive={[
+      {
+        breakpoint: 1280,
+        settings: { slidesToShow: 4, slidesToScroll: 1 },
+      },
+      {
+        breakpoint: 1024,
+        settings: { slidesToShow: 3, slidesToScroll: 1 },
+      },
+      {
+        breakpoint: 768,
+        settings: { slidesToShow: 2, slidesToScroll: 1 },
+      },
+      {
+        breakpoint: 480,
+        settings: { slidesToShow: 1, slidesToScroll: 1 },
+      },
+    ]}
+  >
+    {products.map((product) => (
+      <div key={product.id} className="px-2 py-2 ">
+        <div className="rounded-[10px] hover:scale-103 transition duration-400  pt-[18px] py-[8px] hover:shadow-[2px_2px_0px_rgba(128,128,128,0.5)] border border-gray-400 flex flex-col items-center text-center">
           <img
             src={Men}
             alt={product.title}
-            className="w-30 h-auto object-cover mb-2"
+            className="w-25 h-auto object-cover mb-2 "
           />
           <h2 className="text-lg">{product.title}</h2>
-
-         
-       
         </div>
-      ))}
-    </div>
-          </div>
+      </div>
+    ))}
+  </Slider>
+</div>
 
     </div>
   );
--- src/pages/qqq.jsx
+++ src/pages/qqq.jsx
@@ -22,6 +22,101 @@ export const productList = {
       },
       {
         id: 1,
+        title: "adidas",
+        description: "An apple mobile which is nothing like apple",
+        price: 993,
+        discountPercentage: 12.96,
+        rating: 0,
+        stock: 0,
+        brand: "Apple",
+        category: "smartphones",
+        thumbnail: "https://i.dummyjson.com/data/products/1/thumbnail.jpg",
+        images: [
+          "https://i.dummyjson.com/data/products/1/1.jpg",
+          "https://i.dummyjson.com/data/products/1/2.jpg",
+          "https://i.dummyjson.com/data/products/1/3.jpg",
+          "https://i.dummyjson.com/data/products/1/thumbnail.jpg"
+        ],
+        deleted: true
+      },
+      {
+        id: 1,
+        title: "adidas",
+        description: "An apple mobile which is nothing like apple",
+        price: 993,
+        discountPercentage: 12.96,
+        rating: 0,
+        stock: 0,
+        brand: "Apple",
+        category: "smartphones",
+        thumbnail: "https://i.dummyjson.com/data/products/1/thumbnail.jpg",
+        images: [
+          "https://i.dummyjson.com/data/products/1/1.jpg",
+          "https://i.dummyjson.com/data/products/1/2.jpg",
+          "https://i.dummyjson.com/data/products/1/3.jpg",
+          "https://i.dummyjson.com/data/products/1/thumbnail.jpg"
+        ],
+        deleted: true
+      },
+      {
+        id: 1,
+        title: "adidas",
+        description: "An apple mobile which is nothing like apple",
+        price: 993,
+        discountPercentage: 12.96,
+        rating: 0,
+        stock: 0,
+        brand: "Apple",
+        category: "smartphones",
+        thumbnail: "https://i.dummyjson.com/data/products/1/thumbnail.jpg",
+        images: [
+          "https://i.dummyjson.com/data/products/1/1.jpg",
+          "https://i.dummyjson.com/data/products/1/2.jpg",
+          "https://i.dummyjson.com/data/products/1/3.jpg",
+          "https://i.dummyjson.com/data/products/1/thumbnail.jpg"
+        ],
+        deleted: true
+      },
+      {
+        id: 1,
+        title: "adidas",
+        description: "An apple mobile which is nothing like apple",
+        price: 993,
+        discountPercentage: 12.96,
+        rating: 0,
+        stock: 0,
+        brand: "Apple",
+        category: "smartphones",
+        thumbnail: "https://i.dummyjson.com/data/products/1/thumbnail.jpg",
+        images: [
+          "https://i.dummyjson.com/data/products/1/1.jpg",
+          "https://i.dummyjson.com/data/products/1/2.jpg",
+          "https://i.dummyjson.com/data/products/1/3.jpg",
+          "https://i.dummyjson.com/data/products/1/thumbnail.jpg"
+        ],
+        deleted: true
+      },
+      {
+        id: 1,
+        title: "adidas",
+        description: "An apple mobile which is nothing like apple",
+        price: 993,
+        discountPercentage: 12.96,
+        rating: 0,
+        stock: 0,
+        brand: "Apple",
+        category: "smartphones",
+        thumbnail: "https://i.dummyjson.com/data/products/1/thumbnail.jpg",
+        images: [
+          "https://i.dummyjson.com/data/products/1/1.jpg",
+          "https://i.dummyjson.com/data/products/1/2.jpg",
+          "https://i.dummyjson.com/data/products/1/3.jpg",
+          "https://i.dummyjson.com/data/products/1/thumbnail.jpg"
+        ],
+        deleted: true
+      },
+      {
+        id: 1,
         title: "Nike",
         description: "An apple mobile which is nothing like apple",
         price: 993,
