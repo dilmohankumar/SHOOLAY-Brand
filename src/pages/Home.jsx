b2ecaea32194ec557b9dea540593cc30227a2138
macbook <macbook@macbooks-MBP.lan>
2025-07-12 18:54:48 +0530

    trending products

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,20 +8,19 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import { Autoplay, Pagination } from "swiper/modules";
 
 import { productList } from "./qqq.jsx";
+import { trendingproductList } from "./trending products data.jsx";
 
 import "swiper/css";
 import "swiper/css/pagination";
 
-
-// Import these at the top of your file
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
-
 const Home = () => {
   const banners = [Banner, Banner2];
-  const [products, setProducts] = useState(productList.products);
+  const [products] = useState(productList.products);
+  const [Trendproducts] = useState(trendingproductList.trendproducts);
 
   return (
     <div>
@@ -44,51 +43,90 @@ const Home = () => {
           ))}
         </Swiper>
       </div>
-      {/* ---------------------3 Product -------------------------- */}
-      <div className="m-12">
-  <Slider className=""
-    dots={false}
-    infinite={true}
-    speed={1000}
-    slidesToShow={5}
-    slidesToScroll={1}
-    autoplay={true}
-    autoplaySpeed={2000} // 3 seconds
-    arrows={true}
-    responsive={[
-      {
-        breakpoint: 1280,
-        settings: { slidesToShow: 4, slidesToScroll: 1 },
-      },
-      {
-        breakpoint: 1024,
-        settings: { slidesToShow: 3, slidesToScroll: 1 },
-      },
-      {
-        breakpoint: 768,
-        settings: { slidesToShow: 2, slidesToScroll: 1 },
-      },
-      {
-        breakpoint: 480,
-        settings: { slidesToShow: 1, slidesToScroll: 1 },
-      },
-    ]}
-  >
-    {products.map((product) => (
-      <div key={product.id} className="px-2 py-2 ">
-        <div className="rounded-[10px] hover:scale-103 transition duration-400  pt-[18px] py-[8px] hover:shadow-[2px_2px_0px_rgba(128,128,128,0.5)] border border-gray-400 flex flex-col items-center text-center">
-          <img
-            src={Men}
-            alt={product.title}
-            className="w-25 h-auto object-cover mb-2 "
-          />
-          <h2 className="text-lg">{product.title}</h2>
+      {/* ---------------------brand section -------------------------- */}
+      <div className="m-12 ">
+        <Slider
+          className=""
+          dots={false}
+          infinite={true}
+          speed={1000}
+          slidesToShow={5}
+          slidesToScroll={1}
+          autoplay={true}
+          autoplaySpeed={2000} // 3 seconds
+          arrows={true}
+          responsive={[
+            {
+              breakpoint: 1280,
+              settings: { slidesToShow: 4, slidesToScroll: 1 },
+            },
+            {
+              breakpoint: 1024,
+              settings: { slidesToShow: 3, slidesToScroll: 1 },
+            },
+            {
+              breakpoint: 768,
+              settings: { slidesToShow: 2, slidesToScroll: 1 },
+            },
+            {
+              breakpoint: 480,
+              settings: { slidesToShow: 1, slidesToScroll: 1 },
+            },
+          ]}
+        >
+          {products.map((product) => (
+            <div key={product.id} className="px-2 py-2 ">
+              <div className="cursor-pointer rounded-[10px] hover:scale-103 transition duration-400  pt-[18px] py-[8px] hover:shadow-[2px_2px_0px_rgba(128,128,128,0.5)] border border-gray-400 flex flex-col items-center text-center">
+                <img
+                  src={Men}
+                  alt={product.title}
+                  className="w-25 h-auto object-cover mb-2 "
+                />
+                <h2 className="text-lg">{product.title}</h2>
+              </div>
+            </div>
+          ))}
+        </Slider>
+      </div>
+
+      {/* --------------------trending section------------------ */}
+
+      <div>
+        <div className="mx-12 my-8">
+          <h1 className="text-xl font-semibold  text-gray-800 tracking-wide">
+            Trending Products
+          </h1>
+        </div>
+        <div className="m-12  flex flex-wrap gap-4 min-w-[651px]">
+          {Trendproducts.map((trendproduct) => (
+            <div key={trendproduct.id} className="p-1 basis-[calc(20%-1rem)]">
+              <div className="hover:shadow-md hover:scale-105 transition-transform transition-shadow duration-300 border border-gray-200 w-full">
+                <img
+                  src={trendproduct.thumbnail}
+                  alt={trendproduct.title}
+                  className=" cursor-pointer w-full h-auto object-cover mb-2"
+                />
+                <div className="p-1">
+                  <h3 className="text-base md:text-lg font-semibold text-gray-800 truncate">
+                    {trendproduct.title}
+                  </h3>
+                  <h3 className="cursor-pointer text-sm text-gray-600 h-6 w-20 overflow-hidden text-ellipsis whitespace-nowrap">
+                    {trendproduct.description}
+                  </h3>
+                  <h1 className="cursor-pointer text-sm font-semibold text-pink-600">
+                    ₹{trendproduct.price}
+                  </h1>
+                </div>
+              </div>
+            </div>
+          ))}
         </div>
       </div>
-    ))}
-  </Slider>
-</div>
 
+      {/* -------------------------brand logo----------------------------- */}
+      <div>
+        <div>Brand</div>
+      </div>
     </div>
   );
 };
---
+++ src/pages/trending products data.jsx
@@ -0,0 +1,192 @@
+// productData.js
+export const trendingproductList = {
+    trendproducts: [
+      {
+        id: 1,
+        title: "shoolay",
+        description: "An apple mobile which is nothing like apple",
+        price: 1993,
+        discountPercentage: 12.96,
+        rating: 0,
+        stock: 0,
+        brand: "b",
+        category: "smartphones",
+        thumbnail: "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/i1-665455a5-45de-40fb-945f-c1852b82400d/react-infinity-run-flyknit-mens-running-shoe-zX42Nc.jpg",
+        images: [
+          "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/i1-665455a5-45de-40fb-945f-c1852b82400d/react-infinity-run-flyknit-mens-running-shoe-zX42Nc.jpg",
+         
+        ],
+        deleted: true
+      },
+      {
+        id: 1,
+        title: "shoolay",
+        description: "An apple mobile which is nothing like apple",
+        price: 1993,
+        discountPercentage: 12.96,
+        rating: 0,
+        stock: 0,
+        brand: "b",
+        category: "smartphones",
+        thumbnail: "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/i1-665455a5-45de-40fb-945f-c1852b82400d/react-infinity-run-flyknit-mens-running-shoe-zX42Nc.jpg",
+        images: [
+          "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/i1-665455a5-45de-40fb-945f-c1852b82400d/react-infinity-run-flyknit-mens-running-shoe-zX42Nc.jpg",
+         
+        ],
+        deleted: true
+      },
+      {
+        id: 1,
+        title: "shoolay",
+        description: "An apple mobile which is nothing like apple",
+        price: 993,
+        discountPercentage: 12.96,
+        rating: 0,
+        stock: 0,
+        brand: "Apple",
+        category: "smartphones",
+        thumbnail: "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/i1-665455a5-45de-40fb-945f-c1852b82400d/react-infinity-run-flyknit-mens-running-shoe-zX42Nc.jpg",
+        images: [
+          "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/i1-665455a5-45de-40fb-945f-c1852b82400d/react-infinity-run-flyknit-mens-running-shoe-zX42Nc.jpg"
+         
+        ],
+        deleted: true
+      },
+      {
+        id: 1,
+        title: "shoolay",
+        description: "An apple mobile which is nothing like apple",
+        price: 993,
+        discountPercentage: 12.96,
+        rating: 0,
+        stock: 0,
+        brand: "b",
+        category: "smartphones",
+        thumbnail: "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/i1-665455a5-45de-40fb-945f-c1852b82400d/react-infinity-run-flyknit-mens-running-shoe-zX42Nc.jpg",
+        images: [
+          "https://i.dummyjson.com/data/products/1/1.jpg",
+          "https://i.dummyjson.com/data/products/1/2.jpg",
+          "https://i.dummyjson.com/data/products/1/3.jpg",
+          "https://i.dummyjson.com/data/products/1/thumbnail.jpg"
+        ],
+        deleted: true
+      },
+      {
+        id: 1,
+        title: "shoolay",
+        description: "An apple mobile which is nothing like apple",
+        price: 993,
+        discountPercentage: 12.96,
+        rating: 0,
+        stock: 0,
+        brand: "b",
+        category: "smartphones",
+        thumbnail: "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/i1-665455a5-45de-40fb-945f-c1852b82400d/react-infinity-run-flyknit-mens-running-shoe-zX42Nc.jpg",
+        images: [
+          "https://i.dummyjson.com/data/products/1/1.jpg",
+          "https://i.dummyjson.com/data/products/1/2.jpg",
+          "https://i.dummyjson.com/data/products/1/3.jpg",
+          "https://i.dummyjson.com/data/products/1/thumbnail.jpg"
+        ],
+        deleted: true
+      },
+      {
+        id: 1,
+        title: "shoolay",
+        description: "An apple mobile which is nothing like apple",
+        price: 993,
+        discountPercentage: 12.96,
+        rating: 0,
+        stock: 0,
+        brand: "shoolay",
+        category: "smartphones",
+        thumbnail: "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/i1-665455a5-45de-40fb-945f-c1852b82400d/react-infinity-run-flyknit-mens-running-shoe-zX42Nc.jpg",
+        images: [
+          "https://i.dummyjson.com/data/products/1/1.jpg",
+          "https://i.dummyjson.com/data/products/1/2.jpg",
+          "https://i.dummyjson.com/data/products/1/3.jpg",
+          "https://i.dummyjson.com/data/products/1/thumbnail.jpg"
+        ],
+        deleted: true
+      },
+      {
+        id: 1,
+        title: "shoolay",
+        description: "An apple mobile which is nothing like apple",
+        price: 993,
+        discountPercentage: 12.96,
+        rating: 0,
+        stock: 0,
+        brand: "Apple",
+        category: "smartphones",
+        thumbnail: "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/i1-665455a5-45de-40fb-945f-c1852b82400d/react-infinity-run-flyknit-mens-running-shoe-zX42Nc.jpg",
+        images: [
+          "https://i.dummyjson.com/data/products/1/1.jpg",
+          "https://i.dummyjson.com/data/products/1/2.jpg",
+          "https://i.dummyjson.com/data/products/1/3.jpg",
+          "https://i.dummyjson.com/data/products/1/thumbnail.jpg"
+        ],
+        deleted: true
+      },
+      {
+        id: 1,
+        title: "shoolay",
+        description: "An apple mobile which is nothing like apple",
+        price: 993,
+        discountPercentage: 12.96,
+        rating: 0,
+        stock: 0,
+        brand: "Apple",
+        category: "smartphones",
+        thumbnail: "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/i1-665455a5-45de-40fb-945f-c1852b82400d/react-infinity-run-flyknit-mens-running-shoe-zX42Nc.jpg",
+        images: [
+          "https://i.dummyjson.com/data/products/1/1.jpg",
+          "https://i.dummyjson.com/data/products/1/2.jpg",
+          "https://i.dummyjson.com/data/products/1/3.jpg",
+          "https://i.dummyjson.com/data/products/1/thumbnail.jpg"
+        ],
+        deleted: true
+      },
+      {
+        id: 1,
+        title: "shoolay",
+        description: "An apple mobile which is nothing like apple",
+        price: 993,
+        discountPercentage: 12.96,
+        rating: 0,
+        stock: 0,
+        brand: "Apple",
+        category: "smartphones",
+        thumbnail: "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/i1-665455a5-45de-40fb-945f-c1852b82400d/react-infinity-run-flyknit-mens-running-shoe-zX42Nc.jpg",
+        images: [
+          "https://i.dummyjson.com/data/products/1/1.jpg",
+          "https://i.dummyjson.com/data/products/1/2.jpg",
+          "https://i.dummyjson.com/data/products/1/3.jpg",
+          "https://i.dummyjson.com/data/products/1/thumbnail.jpg"
+        ],
+        deleted: true
+      },
+      {
+        id: 1,
+        title: "shoolay",
+        description: "An apple mobile which is nothing like apple",
+        price: 993,
+        discountPercentage: 12.96,
+        rating: 0,
+        stock: 0,
+        brand: "Apple",
+        category: "smartphones",
+        thumbnail: "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/i1-665455a5-45de-40fb-945f-c1852b82400d/react-infinity-run-flyknit-mens-running-shoe-zX42Nc.jpg",
+        images: [
+          "https://i.dummyjson.com/data/products/1/1.jpg",
+          "https://i.dummyjson.com/data/products/1/2.jpg",
+          "https://i.dummyjson.com/data/products/1/3.jpg",
+          "https://i.dummyjson.com/data/products/1/thumbnail.jpg"
+        ],
+        deleted: true
+      },
+    
+      // ... rest of products
+    ]
+  };
+  
